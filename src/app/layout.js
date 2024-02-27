4245e040d45026c7d54876c27534ba05c49b5dd8
Alexandre Rayol <alexandrerayol.dev@gmail.com>
2024-02-27 22:13:15 +0000

    first commit

---
+++ src/app/layout.js
@@ -0,0 +1,15 @@
+export const metadata = {
+    title: 'Tabnews',
+    description: 'Conteúdos com valor concreto para quem trabalha com tecnologia.'
+}
+
+
+
+export default function RootLayout({ children }) {
+  return (
+    <html>
+      <head />
+      <body>{children}</body>
+    </html>
+  )
+}
---
+++ src/app/page.js
@@ -0,0 +1,19 @@
+'use client'
+import { useState } from "react"
+import styles from "./styles.module.css"
+import {Image} from 'next/image'
+
+export default function Home() {
+    const [isOpen, setIsOpen] = useState(false)
+
+    return (
+        <div className={styles.container}>
+            <div className={isOpen ? styles.open : styles.close}>
+                <img src="https://pbs.twimg.com/media/FEyAOL0X0AYWGan.jpg"/>
+            </div>
+            <button className={styles.button} type="button" onClick={() => setIsOpen(!isOpen)}>
+                {!isOpen ? 'click me' : 'love you'}
+            </button>
+        </div>
+    )
+}
