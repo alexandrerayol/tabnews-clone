89e772c0c6f0e006d5e422e334c225849acf02ff
Alexandre Rayol <alexandrerayol.dev@gmail.com>
2024-03-29 17:41:22 -0300

    add /api/v1/status endpoint

--- src/app/page.js
+++ src/app/page.js
@@ -2,10 +2,6 @@
 import { useState } from "react";
 import styles from "./styles.module.css";
 
-function teste() {
-  console.log("teste");
-}
-
 export default function Home() {
   const [isOpen, setIsOpen] = useState(false);
 
