8db5b80b8ce761a98f36ec061993ebb8962b1646
Alexandre Rayol <alexandrerayol.dev@gmail.com>
2024-04-19 19:48:15 +0000

    Add error handler to query function body

--- infra/database.js
+++ infra/database.js
@@ -1,18 +1,22 @@
 import { Client } from "pg";
-
 async function query(QueryObject) {
   const client = new Client({
     user: process.env.POSTGRES_USER,
     host: process.env.POSTGRES_HOST,
-    database: process.env.POSTGRES_DATABASE,
+    database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
     port: process.env.POSTGRES_PORT,
   });
   await client.connect();
 
-  const response = await client.query(QueryObject);
-  await client.end();
-  return response;
+  try {
+    const response = await client.query(QueryObject);
+    return response;
+  } catch (error) {
+    console.error(error);
+  } finally {
+    await client.end();
+  }
 }
 
 export default {
